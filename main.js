34cd9c87a26e9f94046e56df59809ff537af2780
Edu Navarre <eduardo.navarre.montero@gmail.com>
2024-12-05 10:12:40 -0400

    add navbar and project cards interaction

--- main.js
+++ main.js
@@ -1,2 +1,32 @@
-// const navbar = document.querySelector('.nav-bar ul');
-// navbar.style.backgroundColor = '#393b44fa';
+const navbar = document.querySelector('.nav-bar ul');
+
+function unhideBackground() {
+    // Check if the window is NOT scrolled to the top
+    if (window.scrollY !== 0) {
+        navbar.style.backgroundColor = '#373941ef';
+    } else {
+        navbar.style.backgroundColor = '#1C1D21'; // OG background color
+    }
+}
+
+window.addEventListener('scroll', unhideBackground);
+// Initial check in case the page is loaded at the top
+unhideBackground();
+
+// --------------------------------------------------
+
+const projects = document.querySelectorAll('.project');
+
+function moveProject() {
+    this.style.top = '5px';
+}
+
+function resetProject() {
+    this.style.top = '0';
+}
+
+// Loop through each .project elem and adds event listener
+projects.forEach(project => {
+    project.addEventListener('mouseover', moveProject);
+    project.addEventListener('mouseout', resetProject);
+});
